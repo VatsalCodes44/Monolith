f76c2651638cc86f6c9dac232f24eefe01e7e190
Vatsal Mahajan <mahajanvatsal44@gmail.com>
2026-03-06 05:39:14 +0530

    testing

--- chess-on-chain/app/_layout.tsx
+++ chess-on-chain/app/_layout.tsx
@@ -1,9 +1,9 @@
-import { StyleSheet, Text, View } from 'react-native'
 import { Stack } from 'expo-router'
 import * as SplashScreen from 'expo-splash-screen';
 import { Orbitron_900Black, useFonts } from '@expo-google-fonts/orbitron';
-import { useEffect } from 'react';
+import { useCallback, useEffect } from 'react';
 import { GestureHandlerRootView } from 'react-native-gesture-handler';
+import { useAssets } from 'expo-asset';
 
 SplashScreen.preventAutoHideAsync();
 
@@ -11,20 +11,19 @@ export default function () {
     const [loaded, error] = useFonts({
         Orbitron_900Black,
     });
+    const [assets] = useAssets([require('../assets/image/homescreen.jpg')]);
 
-    useEffect(() => {
-        if (loaded || error) {
-        SplashScreen.hideAsync();
+    const onLayoutRootView = useCallback(async () => {
+        if ((loaded || error) && assets) {
+            await SplashScreen.hideAsync();
         }
-    }, [loaded, error]);
+    }, [loaded, error, assets]);
 
-    if (!loaded && !error) return null;
+    if ((!loaded && !error) || !assets) return null;
 
     return(
-        <GestureHandlerRootView style={{ flex: 1 }}>
+        <GestureHandlerRootView style={{ flex: 1 }} onLayout={onLayoutRootView}>
             <Stack screenOptions={{ headerShown: false }} />
         </GestureHandlerRootView>
     )
 }
-
-const styles = StyleSheet.create({})
